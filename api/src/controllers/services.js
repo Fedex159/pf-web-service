040428c3d0e672c90b15172a9e17403ddbeeb5ee
jonatansegovia <jsegovia.ush@gmail.com>
2021-10-25 20:02:13 -0300

    api/services/POST userId asociado a servicio

--- api/src/controllers/services.js
+++ api/src/controllers/services.js
@@ -1,4 +1,4 @@
-const { Service } = require('../db.js');
+const { Service, Users } = require('../db.js');
 
 //por cada ruta un controler
 function getServices(req, res) {
@@ -7,15 +7,26 @@ function getServices(req, res) {
 
 async function postServices(req, res, next) {
   try {
-    const { title, img, description, price } = req.body;
+    //userName eventualmente debería ser enviada por cookie
+    const { title, img, description, price, userName } = req.body;
 
+    //busco el user que lo creó para asociárselo
+    const userFound = await Users.findOne({
+      where: {
+        username: userName,
+      },
+    });
+
+    //creo el servicio
     const serviceCreated = await Service.create({
       title,
       img,
       description,
       price,
+      userId: userFound.id,
     });
 
+    //asocio el servicio creado al user que lo creó
     return res.status(200).send(serviceCreated);
   } catch (e) {
     next(e);
--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -3,40 +3,46 @@ const { DataTypes, UUIDV4 } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('users', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  sequelize.define(
+    'users',
+    {
+      id: {
+        // type: DataTypes.UUID,
+        // defaultValue: DataTypes.UUIDV4,
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      // userImg: {
+      //   type: DataTypes.STRING,
+      // },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      location: {
+        // type: DataTypes.ARRAY(DataTypes.STRING),
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    userImg: {
-      type: DataTypes.STRING,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    location: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
