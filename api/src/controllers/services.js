c1fc785716cbed42dc1735873e4be1905298642a
jonatansegovia <jsegovia.ush@gmail.com>
2021-10-25 18:14:13 -0300

    api/services/POST carga de datos en db OK

--- api/src/controllers/services.js
+++ api/src/controllers/services.js
@@ -1,10 +1,25 @@
+const { Service } = require('../db.js');
+
 //por cada ruta un controler
 function getServices(req, res) {
-  res.send("hola");
+  res.send('hola');
 }
 
-function postServices(req, res) {
-  res.send("hola");
+async function postServices(req, res, next) {
+  try {
+    const { title, img, description, price } = req.body;
+
+    const serviceCreated = await Service.create({
+      title,
+      img,
+      description,
+      price,
+    });
+
+    return res.status(200).send(serviceCreated);
+  } catch (e) {
+    next(e);
+  }
 }
 
 module.exports = {
