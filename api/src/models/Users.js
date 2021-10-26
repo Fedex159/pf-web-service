b2b580782d7ea57236faa471b171955d2491ed33
jonatansegovia <jsegovia.ush@gmail.com>
2021-10-26 19:00:13 -0300

    fix-api/POST_purchase modifiqué force a true y modelo

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -5,10 +5,8 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('users', {
     id: {
-      // type: DataTypes.UUID,
-      // defaultValue: DataTypes.UUIDV4,
-      //por si me olvido lo comentado es lo que va, tenía que probar algo y el UUID me está volviendo loco J0n
-      type: DataTypes.STRING,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
     userImg: {
