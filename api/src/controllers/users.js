133aa80f799f5a60acdc2a5a9e2e298be97ed2cb
jonatansegovia <jsegovia.ush@gmail.com>
2021-10-26 17:27:55 -0300

    feat-api/POST_purchase rutas necesarias creadas

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -1,5 +1,5 @@
-const { Users, Service } = require("../db");
-const { validateUser, checkUnique } = require("../utils/validUser");
+const { Users, Service } = require('../db');
+const { validateUser, checkUnique } = require('../utils/validUser');
 
 async function userCreated(req, res, next) {
   try {
@@ -10,7 +10,7 @@ async function userCreated(req, res, next) {
       const errors = validateUser(req.body);
       if (!Object.keys(errors).length) {
         await Users.create(req.body);
-        res.json({ response: "created" }); // responde con 200, y created
+        res.json({ response: 'created' }); // responde con 200, y created
       } else {
         res.status(400).json({ response: errors });
         // algun parametro invalido.
@@ -18,7 +18,7 @@ async function userCreated(req, res, next) {
     } else {
       res
         .status(400)
-        .json({ response: "username or email already exist or is empty" });
+        .json({ response: 'username or email already exist or is empty' });
     }
   } catch (e) {
     next(e);
@@ -29,28 +29,36 @@ async function getUsers(req, res, next) {
   try {
     // let { username } = req.params;
     let { username } = req.query;
-    if(!username){
+    if (!username) {
       const usersDb = await Users.findAll({
-        include: [{
-          model: Service,
-          attributes: [],
-        },],
+        include: [
+          {
+            model: Service,
+            attributes: [],
+          },
+        ],
       });
-    usersDb.length > 0 ? res.status(200).send(usersDb): res.status(500).send({ response: "no users yet"})
-    } else { const userFinded = await Users.findOne({
-      where: {
-        username,
-      }, include: [{
-        model: Service,
-        attributes: [],
-      },],
-    });
-    res.status(200).send(userFinded);
-  }; 
+      usersDb.length > 0
+        ? res.status(200).send(usersDb)
+        : res.status(500).send({ response: 'no users yet' });
+    } else {
+      const userFinded = await Users.findOne({
+        where: {
+          username,
+        },
+        include: [
+          {
+            model: Service,
+            attributes: [],
+          },
+        ],
+      });
+      res.status(200).send(userFinded);
+    }
   } catch (e) {
     next(e);
   }
-};
+}
 
 async function userBanned(req, res, next) {
   try {
@@ -60,22 +68,29 @@ async function userBanned(req, res, next) {
       where: { id: id },
     });
     if (usersInDb === null) {
-      res.json({ respones: "user not founded" });
+      res.json({ respones: 'user not founded' });
     } else {
-      await Users.update({ // si existe seteo el ban en true
-        ban: true
-      }, {
-        where: {id: id}
-      });
-      res.json({ response: "user banned" });
+      await Users.update(
+        {
+          // si existe seteo el ban en true
+          ban: true,
+        },
+        {
+          where: { id: id },
+        }
+      );
+      res.json({ response: 'user banned' });
     }
   } catch (e) {
     next(e);
   }
 }
 
+function postPurchase(req, res, next) {}
+
 module.exports = {
   userCreated,
   userBanned,
-  getUsers
+  getUsers,
+  postPurchase,
 };
