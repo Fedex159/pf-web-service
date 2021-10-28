b02620b8e17952267c7300732193f05881c3828b
jonatansegovia <jsegovia.ush@gmail.com>
2021-10-28 06:45:13 -0300

    feat/cliente/sidebar BtnDropDown funcionando

---
+++ client/src/components/SideBar/SideBarBtnDropDown/SideBarBtnDropDown.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeRepairServiceRoundedIcon from '@mui/icons-material/HomeRepairServiceRounded';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import Box from '@mui/material/Box';
+
+export default function SideBarBtnDropDown({ text }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Box>
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <HomeRepairServiceRoundedIcon />
+        </ListItemIcon>
+        <ListItemText primary={text} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemText primary="INTERNO" />
+          </ListItemButton>
+        </List>
+      </Collapse>
+    </Box>
+  );
+}
