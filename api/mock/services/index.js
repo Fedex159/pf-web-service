44ada2259a4f2546ce6904d1dad047df010e6d40
wikonarider <wikonarider@gmail.com>
2021-10-29 18:13:33 -0300

    add mock/transport

--- api/mock/services/index.js
+++ api/mock/services/index.js
@@ -1,8 +1,9 @@
-const { Service } = require('../../src/db');
-const { lessons } = require('./lessons');
-const { repair } = require('./repairs');
+const { Service } = require("../../src/db");
+const { lessons } = require("./lessons");
+const { repair } = require("./repairs");
+const { transport } = require("./transport");
 
-const bulk = [...lessons, ...repair];
+const bulk = [...lessons, ...repair, ...transport];
 
 function loadServices() {
   return Service.bulkCreate(bulk);
