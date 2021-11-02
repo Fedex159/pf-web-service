5eb9e55bf16b887ace444edeb8478d0f5aac4500
jonatansegovia <jsegovia.ush@gmail.com>
2021-11-02 19:57:46 -0300

    feat/client/Checkout state cart recibido en /checkout

--- client/src/components/CheckoutDetail/CheckoutDetail.jsx
+++ client/src/components/CheckoutDetail/CheckoutDetail.jsx
@@ -10,30 +10,79 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
 
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+});
+
 export default function CheckoutDetail() {
   const cart = useSelector((state) => state.cart);
 
   console.log(cart);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar
-          sx={{ mr: 2, display: 'flex', justifyContent: 'space-between' }}
-        >
-          <IconButton
-            component={Link}
-            to="/home"
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar
+            sx={{ mr: 2, display: 'flex', justifyContent: 'space-between' }}
           >
-            <HomeIcon />
-          </IconButton>
-          <UserMenu />
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <IconButton
+              component={Link}
+              to="/home"
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <HomeIcon />
+            </IconButton>
+            <UserMenu />
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Paper sx={{ p: 2, margin: 'auto', maxWidth: 500, flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase sx={{ width: 128, height: 128 }}>
+              <Img alt="complex" src="/static/images/grid/complex.jpg" />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" component="div">
+                  Standard license
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                $19.00
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
   );
 }
