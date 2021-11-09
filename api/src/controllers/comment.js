e38076e2f56824a9ea301dbb15ba0456331171eb
stobar93 <sebastiantobarquintero@gmail.com>
2021-11-09 17:43:40 -0500

    Fix getComment controller

--- api/src/controllers/comment.js
+++ api/src/controllers/comment.js
@@ -3,8 +3,8 @@ const { Qualification } = require("../db.js");
 async function getComment(req, res, next) {
   const { id } = req.query;
   if (id) {
-    const comments = await Qualification.findByPk(id)({
-      attributes: ["comment"],
+    const comments = await Qualification.findByPk(id, {
+      attributes: ["comment", "score", "id", "serviceId", "userId"],
     });
     return res.send(comments);
   }
